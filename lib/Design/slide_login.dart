680cadd7a4e108b08b3e8ee5f8c16d5ce36bee41
Leny00 <lenyg675@gmail.com>
2024-02-08 09:01:15 -0400

    creacion de carpeta iconos e imagenes

---
+++ lib/Design/slide_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../Colors/colors.dart';
+
+class SlideLogin extends StatelessWidget {
+  const SlideLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.textColor,
+      ),
+    );
+  }
+}
