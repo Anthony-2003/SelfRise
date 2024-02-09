ec660e1f9ddb5738620588338fcda83c8fb0e2de
Engels <engelsmlb@gmail.com>
2024-02-08 21:39:16 -0400

    Firt commit

--- lib/Design/menu_principal.dart
+++ lib/Design/menu_principal.dart
@@ -7,32 +7,33 @@ import '../Colors/colors.dart';
 import '../services/api_frase_diaria.dart';
 
 class PantallaMenuPrincipal extends StatelessWidget {
-  const PantallaMenuPrincipal({
-    Key ? key
-  }): super(key: key);
+  const PantallaMenuPrincipal({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 30),
-                  _construirBotonMenu(),
-                  _construirTextoBienvenida(),
-                  _construirTextoSentimientos(),
-                  _construirFilaIconosSentimientos(),
-                  const Text(
-                      'Frase de hoy',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: AppColors.textColor),
-                    ),
-                    _construirFutureBuilder(),
-              ],
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              _construirBotonMenu(),
+              _construirTextoBienvenida(),
+              _construirTextoSentimientos(),
+              _construirFilaIconosSentimientos(),
+              const Text(
+                'Frase de hoy',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.textColor),
+              ),
+              _construirFutureBuilder(),
+            ],
           ),
+        ),
       ),
       bottomNavigationBar: _construirNavigationBarInferior(),
     );
@@ -41,11 +42,11 @@ class PantallaMenuPrincipal extends StatelessWidget {
   Widget _construirBotonMenu() {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10.0),
-        child: IconButton(
-          icon: const Icon(Icons.menu, size: 35, color: AppColors.textColor),
-            onPressed: () {},
-            key: const Key('menu_button'),
-        ),
+      child: IconButton(
+        icon: const Icon(Icons.menu, size: 35, color: AppColors.textColor),
+        onPressed: () {},
+        key: const Key('menu_button'),
+      ),
     );
   }
 
@@ -64,7 +65,10 @@ class PantallaMenuPrincipal extends StatelessWidget {
       padding: EdgeInsets.only(bottom: 8.0),
       child: Text(
         '¿Cómo te sientes hoy?',
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: AppColors.textColor),
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: AppColors.textColor),
       ),
     );
   }
@@ -72,53 +76,55 @@ class PantallaMenuPrincipal extends StatelessWidget {
   Widget _construirIconoConTexto(String nombreAsset, String texto) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/$nombreAsset',
-              width: 50,
-              height: 50,
-            ),
-            const SizedBox(height: 5), // Espacio entre la imagen y el texto
-              Text(
-                texto,
-                style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-              ),
-          ],
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/$nombreAsset',
+            width: 50,
+            height: 50,
+          ),
+          const SizedBox(height: 5), // Espacio entre la imagen y el texto
+          Text(
+            texto,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _construirFilaIconosSentimientos() {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _construirIconoConTexto('feliz.gif', 'Feliz'),
-            _construirIconoConTexto('angel.gif', 'Tranquilo'),
-            _construirIconoConTexto('triste.gif', 'Triste'),
-            _construirIconoConTexto('neutral.gif', 'Neutral'),
-            _construirIconoConTexto('enojado.gif', 'Enojado'),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _construirIconoConTexto('feliz.gif', 'Feliz'),
+          _construirIconoConTexto('angel.gif', 'Tranquilo'),
+          _construirIconoConTexto('triste.gif', 'Triste'),
+          _construirIconoConTexto('neutral.gif', 'Neutral'),
+          _construirIconoConTexto('enojado.gif', 'Enojado'),
+        ],
+      ),
     );
   }
+
+  //HOLA FIRST COMMIT
 
   Widget _construirNavigationBarInferior() {
     return CurvedNavigationBar(
       backgroundColor: Colors.white,
       color: const Color.fromARGB(255, 104, 174, 240),
-        animationDuration: const Duration(milliseconds: 300),
-          height: 150,
-          items: < Widget > [
-            _construirNavigationBarItem(Icons.home),
-            _construirNavigationBarItem(Icons.chat),
-            _construirNavigationBarItem(Icons.assignment),
-            _construirNavigationBarItem(Icons.track_changes),
-            _construirNavigationBarItem(Icons.person),
-          ],
+      animationDuration: const Duration(milliseconds: 300),
+      height: 50,
+      items: <Widget>[
+        _construirNavigationBarItem(Icons.home),
+        _construirNavigationBarItem(Icons.chat),
+        _construirNavigationBarItem(Icons.assignment),
+        _construirNavigationBarItem(Icons.track_changes),
+        _construirNavigationBarItem(Icons.person),
+      ],
     );
   }
 
@@ -134,7 +140,7 @@ class PantallaMenuPrincipal extends StatelessWidget {
   }
 
   Widget _construirFutureBuilder() {
-    return FutureBuilder < Map < String, dynamic >> (
+    return FutureBuilder<Map<String, dynamic>>(
       future: ApiFraseDiaria.fetchData(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -145,7 +151,6 @@ class PantallaMenuPrincipal extends StatelessWidget {
           }
           return _construirTextoError('Error al cargar la frase del día');
         } else {
-
           final frase = snapshot.data!['quote'];
           final autor = snapshot.data!['author'];
           return _traducirFrase(frase, autor);
@@ -163,7 +168,7 @@ class PantallaMenuPrincipal extends StatelessWidget {
   }
 
   Widget _traducirFrase(String frase, String autor) {
-    return FutureBuilder < Map < String, dynamic >> (
+    return FutureBuilder<Map<String, dynamic>>(
       future: ApiTraductor.traducirFrase(frase),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -183,41 +188,48 @@ class PantallaMenuPrincipal extends StatelessWidget {
       child: Center(
         child: Container(
           margin: const EdgeInsets.only(top: 20, bottom: 20),
-            decoration: BoxDecoration(
-              color: AppColors.darkGray,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+          decoration: BoxDecoration(
+            color: AppColors.darkGray,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                fraseDelDia,
+                style: const TextStyle(
+                    color: AppColors.textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Text(
-                    fraseDelDia,
-                    style: const TextStyle(color: AppColors.textColor, fontSize: 20, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
+                    "-$autor",
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 98, 168, 233),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "-$autor",
-                        style: const TextStyle(color: Color.fromARGB(255, 98, 168, 233), fontSize: 20, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                    IconButton(
-                      icon: const Icon(
-                          Icons.share,
-                          color: AppColors.textColor,
-                          size: 30,
-                        ),
-                        onPressed: () {
-                          _compartirFrase(fraseDelDia, autor);
-                        },
-                    ),
                 ],
               ),
+              const SizedBox(height: 16),
+              IconButton(
+                icon: const Icon(
+                  Icons.share,
+                  color: AppColors.textColor,
+                  size: 30,
+                ),
+                onPressed: () {
+                  _compartirFrase(fraseDelDia, autor);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -226,5 +238,4 @@ class PantallaMenuPrincipal extends StatelessWidget {
   void _compartirFrase(String texto, String autor) {
     Share.share("$texto\n-$autor", subject: 'Compartir frase del día');
   }
-
 }
