9aa2be60d19f1006e1126eab6a34345e616b7394
Engels <engelsmlb@gmail.com>
2024-03-02 12:23:27 -0400

    MENU LATERA AVANZADO

---
+++ lib/services/frases_motivacionales.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FrasesMotivacionales {
+  static Future<Map<String, dynamic>> obtenerFraseAleatoria() async {
+    try {
+      String jsonString =
+          await rootBundle.loadString('lib/data/frases_motivacionales.json');
+      Map<String, dynamic> jsonData = json.decode(jsonString);
+      List<dynamic> frases = jsonData['frases_motivacionales'];
+
+      Random random = Random();
+      int index = random.nextInt(frases.length);
+
+      Map<String, dynamic> fraseAleatoria = frases[index];
+
+      return fraseAleatoria;
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+}
