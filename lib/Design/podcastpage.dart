8039d733f703ddd30c6a0a5b9667c8e38d15be5c
Engels <engelsmlb@gmail.com>
2024-04-10 01:21:20 -0400

    modal emociones

--- lib/Design/podcastpage.dart
+++ lib/Design/podcastpage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_proyecto_final/Colors/colors.dart';
 import 'package:flutter_proyecto_final/Design/menu_principal.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_proyecto_final/Design/podcastcontroller.dart';
-import 'package:flutter_proyecto_final/Design/podcastinfoview.dart';
 import 'package:flutter_proyecto_final/data/podcast_data.dart';
+import 'package:flutter_proyecto_final/components/cardwidget.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class PodcastPage extends StatefulWidget {
   const PodcastPage({Key? key}) : super(key: key);
@@ -14,246 +14,151 @@ class PodcastPage extends StatefulWidget {
 }
 
 class _PodcastPageState extends State<PodcastPage> {
+  String _selectedCategory = "Motivación"; // Categoría predeterminada al inicio
+
+  List<cardsdata> _filteredPodcasts() {
+    return podcascards
+        .where((podcast) => podcast.categoria == _selectedCategory)
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Filtrar los elementos de la lista según su ID
-    List<cards> motivacionList =
-        carousel.where((element) => element.categoria == "motivacion").toList();
-    List<cards> saludMentalList = carousel
-        .where((element) => element.categoria == "salud-mental")
-        .toList();
-    List<cards> DesarrolloPersonalList = carousel
-        .where((element) => element.categoria == "DesarrolloPersonal")
-        .toList();
-    List<cards> BienestarFisicolList = carousel
-        .where((element) => element.categoria == "BienestarFisico")
-        .toList();
+    Future<void>? _launched;
+
+    List<cardsdata> filteredPodcasts = _filteredPodcasts();
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.textColor,
-          automaticallyImplyLeading: false,
-          title: Text(
-            'Podcasts',
-            style: TextStyle(
-                color: AppColors.appcolor,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-                fontFamily: AutofillHints.sublocality),
-          ),
-          centerTitle: true,
-          // Agregamos un IconButton en la AppBar para la flecha de regreso
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
+      appBar: AppBar(
+        backgroundColor: AppColors.textColor,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Podcasts',
+          style: TextStyle(
               color: AppColors.appcolor,
-            ), // Icono de flecha hacia atrás
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      PantallaMenuPrincipal(), // Aquí se crea la instancia de BookListScreen
-                ),
-              );
-            },
-          ),
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              fontFamily: AutofillHints.sublocality),
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              // Agrega aquí el contenido de la página de podcast
-              children: [
-                Container(
-                  margin: EdgeInsets.only(
-                    top: 30,
-                  ),
-                  child: Text(
-                    'Podcast recomendados',
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AutofillHints.sublocality),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width *
-                      0.8, // Por ejemplo, el 80% del ancho de la pantalla
-                  child: Divider(
-                    color: AppColors.textColor,
-                    thickness: 1.0,
-                    height: 20.0,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 40, bottom: 5),
-                  child: Text(
-                    'MOTIVACION',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AutofillHints.sublocality),
-                  ),
-                ),
-                // ****** CARRUSEL MOTIVACION *****
-                Container(
-                  color: AppColors.textColor,
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                  child: CarouselSlider.builder(
-                      itemCount: motivacionList.length,
-                      itemBuilder: (context, index, realIndex) {
-                        final carruselImage = carousel[index];
-                        return cardImages(
-                          card: motivacionList[index],
-                        );
-                      },
-                      options: CarouselOptions(
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.8,
-                        autoPlay: true,
-                        autoPlayInterval: Duration(seconds: 6),
-                        pauseAutoPlayOnTouch: true,
-                        enableInfiniteScroll: true,
-                        enlargeCenterPage: true,
-                      )),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 40, bottom: 5),
-                  child: Text(
-                    'SALUD MENTAL',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AutofillHints.sublocality),
-                  ),
-                ),
-                // ****** CARRUSEL SALUD MENTAL *****
-                Container(
-                  color: AppColors.textColor,
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                  child: CarouselSlider.builder(
-                      itemCount: saludMentalList.length,
-                      itemBuilder: (context, index, realIndex) {
-                        final carruselImage = carousel[index];
-                        return cardImages(
-                          card: saludMentalList[index],
-                        );
-                      },
-                      options: CarouselOptions(
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.8,
-                        autoPlay: true,
-                        autoPlayInterval: Duration(seconds: 6),
-                        pauseAutoPlayOnTouch: true,
-                        enableInfiniteScroll: true,
-                        enlargeCenterPage: true,
-                      )),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 40, bottom: 5),
-                  child: Text(
-                    'DESARROLLO PERSONAL',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AutofillHints.sublocality),
-                  ),
-                ),
-                // ****** CARRUSEL DESARROLLO PERSONAL *****
-                Container(
-                  color: AppColors.textColor,
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                  child: CarouselSlider.builder(
-                      itemCount: DesarrolloPersonalList.length,
-                      itemBuilder: (context, index, realIndex) {
-                        final carruselImage = carousel[index];
-                        return cardImages(
-                          card: DesarrolloPersonalList[index],
-                        );
-                      },
-                      options: CarouselOptions(
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.8,
-                        autoPlay: true,
-                        autoPlayInterval: Duration(seconds: 6),
-                        pauseAutoPlayOnTouch: true,
-                        enableInfiniteScroll: true,
-                        enlargeCenterPage: true,
-                      )),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 40, bottom: 5),
-                  child: Text(
-                    'BIENESTAR FISICO',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AutofillHints.sublocality),
-                  ),
-                ),
-                // ****** CARRUSEL BIENESTAR FISICO *****
-                Container(
-                  color: AppColors.textColor,
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                  child: CarouselSlider.builder(
-                      itemCount: BienestarFisicolList.length,
-                      itemBuilder: (context, index, realIndex) {
-                        final carruselImage = carousel[index];
-                        return cardImages(
-                          card: BienestarFisicolList[index],
-                        );
-                      },
-                      options: CarouselOptions(
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.8,
-                        autoPlay: true,
-                        autoPlayInterval: Duration(seconds: 6),
-                        pauseAutoPlayOnTouch: true,
-                        enableInfiniteScroll: true,
-                        enlargeCenterPage: true,
-                      )),
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
-}
-
-class cardImages extends StatelessWidget {
-  final cards card;
-  const cardImages({
-    Key? key,
-    required this.card,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: InkWell(
-          onTap: () {
-            card.copy();
+        centerTitle: true,
+        // Agregamos un IconButton en la AppBar para la flecha de regreso
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: AppColors.appcolor,
+          ), // Icono de flecha hacia atrás
+          onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => infopodcast(
-                          carousel: card,
-                        )));
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    PantallaMenuPrincipal(), // Aquí se crea la instancia de BookListScreen
+              ),
+            );
           },
-          child: FadeInImage(
-            placeholder: AssetImage("assets/podcast-carousel/loading.gif"),
-            image: AssetImage(
-              card.image,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              child: Text(
+                "Podcast recomendados",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
             ),
-            fit: BoxFit.fill,
-          ),
+            Container(
+              width: 300,
+              child: Divider(
+                thickness: 2,
+                color: AppColors
+                    .textColor, // Cambiado a color gris para la división
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 110, vertical: 10),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  value: _selectedCategory,
+                  items: [
+                    "Motivación",
+                    "Salud Mental",
+                    "Desarrollo Personal",
+                    "Bienestar Físico"
+                  ].map((category) {
+                    return DropdownMenuItem(
+                      value: category,
+                      child: Center(
+                        child: Text(
+                          category,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedCategory = value!;
+                    });
+                  },
+                  buttonStyleData: ButtonStyleData(
+                    height: 50,
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 14, right: 14),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      border: Border.all(
+                        color: AppColors.drawer,
+                      ),
+                      color: AppColors.drawer,
+                    ),
+                    elevation: 2,
+                  ),
+                  iconStyleData: const IconStyleData(
+                    icon: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                    ),
+                    iconSize: 14,
+                    iconEnabledColor: Colors.yellow,
+                    iconDisabledColor: Colors.grey,
+                  ),
+                  dropdownStyleData: DropdownStyleData(
+                    maxHeight: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: AppColors.textColor,
+                    ),
+                    offset: const Offset(-0, 0),
+                    scrollbarTheme: ScrollbarThemeData(
+                      radius: const Radius.circular(40),
+                      thickness: MaterialStateProperty.all(6),
+                      thumbVisibility: MaterialStateProperty.all(true),
+                    ),
+                  ),
+                  menuItemStyleData: const MenuItemStyleData(
+                    height: 40,
+                    padding: EdgeInsets.only(left: 14, right: 14),
+                  ),
+                ),
+              ),
+            ),
+            // Mostrar las tarjetas filtradas según la categoría seleccionada
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredPodcasts.length,
+                itemBuilder: (context, index) {
+                  return CardWidget(podcast: filteredPodcasts[index]);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
